0928ab4258deb233f0f1c049dccf12821764a6f0
pamphilemkp <pamphilemkp@gmail.com>
2022-09-27 20:16:20 +0300

    update files

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@
 SELECT * from animals WHERE name like '%mon';
 
 /*List the name of all animals born between 2016 and 2019*/
-SELECT name from animals WHERE date_of_birth,escape BETWEEN '2016-01-01' - '2019-12-31';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 /*List the name of all animals that are neutered and have less than 3 escape attempts*/
 SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
