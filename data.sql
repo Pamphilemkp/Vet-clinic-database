e8b854a14207069c0869db9e02a3013806de9fc1
pamphilemkp <pamphilemkp@gmail.com>
2022-09-30 12:36:01 +0300

    query multiple tables

--- data.sql
+++ data.sql
@@ -7,7 +7,7 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
  VALUES (3,'Pikachu','2021-01-07',1,FALSE,15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
- VALUES (4,' Devimon','2017-05-12',5,TRUE,11.00);
+ VALUES (4,'Devimon','2017-05-12',5,TRUE,11.00);
 
  /*Insert the following data: */
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
@@ -30,4 +30,33 @@ values (10,'Blossom','1998-10-13',3,TRUE,17.00);
 
  INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 values (11,'Ditto','2022-05-14',4,TRUE,17.00);
+
+
+
+--  INSERTING data into the owners table: 
+
+INSERT INTO owners (full_name, age)
+ VALUES
+    ('Sam Smith', 34), 
+    ('Jennifer Orwell', 19),
+    ('Bob ', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+ --  INSERTING data into the species table: 
+ INSERT INTO species (name)
+ VALUES ('Pokemon'), ('Digimon');
+
+--  Updating the animals table so if the name of the animal ends with 'mon' the species_id it is 2 (value of Digimon in species table)
+--  or otherwise it is 2;
+UPDATE animals SET species_id = 2 WHERE name like '%mon';
+UPDATE animals SET species_id = 1 WHERE name NOT like '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id): 
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' or name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' or name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' or name = 'Squirtle' or name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' or name = 'Boarmon';
 
--- schema.sql
+++ schema.sql
@@ -11,3 +11,42 @@ CREATE TABLE animals (
 
 /*Add a column species of type string to your animals table. Modify your schema.sql file*/
 ALTER TABLE animals ADD species varchar(100);
+
+/*create owners table*/
+CREATE TABLE owners (
+    id int GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age int,
+    PRIMARY KEY(id)
+);
+
+
+/*create species table*/
+CREATE TABLE species (
+    id int GENERATED ALWAYS AS IDENTITY,
+    name text,
+    PRIMARY KEY(id)
+);
+
+-- Remove the species column from the table animals
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add species_id from the table animals
+ALTER TABLE animals ADD species_id INT;
+
+--  Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ ADD CONSTRAINT fk_species
+  FOREIGN KEY (species_id)
+  REFERENCES species (id)
+  ON DELETE SET NULL;
+
+  -- Add owner_id from the table animals
+ALTER TABLE animals ADD owner_id INT;
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ ADD CONSTRAINT fk_owners
+  FOREIGN KEY (owner_id)
+  REFERENCES owners (id)
+  ON DELETE SET NULL;
